c2104d2312fcf65043235cc122c10a933de4013e
Luciano <lucianodeciopacheco@gmail.com>
2024-07-27 13:59:44 -0300

    Iniciando Projeto

--- my-sales-app/src/app/app.component.ts
+++ my-sales-app/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-sales-app';
 }
