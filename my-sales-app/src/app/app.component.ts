4120247aa6a0e17973b2079f75aca9281def5c12
Luciano <lucianodeciopacheco@gmail.com>
2024-07-28 14:11:33 -0300

    Menu

--- my-sales-app/src/app/app.component.ts
+++ my-sales-app/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CommonModule} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatSlideToggleModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
