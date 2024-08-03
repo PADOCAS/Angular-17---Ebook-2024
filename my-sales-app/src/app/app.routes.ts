ba62adc78de2e7e559e1847d95ee68bc1d32a6ea
Luciano <lucianodeciopacheco@gmail.com>
2024-08-03 14:03:47 -0300

    Dashboard

--- my-sales-app/src/app/app.routes.ts
+++ my-sales-app/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import {CategoriasComponent} from "./categorias/categorias.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
-export const routes: Routes = [{
-  path: 'categorias',
-  component: CategoriasComponent
-}];
+export const routes: Routes = [
+  {
+    path: 'categorias',
+    component: CategoriasComponent
+  },
+  {
+    path: '',
+    component: DashboardComponent
+  }
+];
