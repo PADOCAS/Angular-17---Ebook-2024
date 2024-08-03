40ab4caf79a1594458c1289be51d51b16d103ff2
Luciano <lucianodeciopacheco@gmail.com>
2024-08-03 13:31:23 -0300

    Criação do Menu

--- my-sales-app/src/app/home/home.component.ts
+++ my-sales-app/src/app/home/home.component.ts
@@ -1,13 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {Component, inject} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {AsyncPipe} from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+import {MenuComponent} from "../menu/menu.component";
 
 @Component({
   selector: 'app-home',
@@ -21,24 +22,10 @@ import { map, shareReplay } from 'rxjs/operators';
     MatListModule,
     MatIconModule,
     AsyncPipe,
+    MenuComponent,
   ]
 })
 export class HomeComponent {
-  menuItems: Array<{path: string; label: string}> = [
-    {
-      path: '/',
-      label: 'Início'
-    },
-    {
-      path: '/categorias',
-      label: 'Categorias'
-    },
-    {
-      path: '/fornecedores',
-      label: 'Fornecedores'
-    }
-  ];
-
   private breakpointObserver = inject(BreakpointObserver);
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
