526b7ddb47e68e28c1f726ad541ee6cff0da29ef
Luciano <lucianodeciopacheco@gmail.com>
2024-10-06 16:36:30 -0300

    teste

--- my-sales-app/src/app/app.config.ts
+++ my-sales-app/src/app/app.config.ts
@@ -4,10 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideHttpClient} from '@angular/common/http';
-import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(),
-    { provide: LocationStrategy, useClass: HashLocationStrategy } // Adiciona HashLocationStrategy
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
 };
