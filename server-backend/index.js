c90883bcaa87f66ee9cb1b758428e43367761390
Luciano Decio Pacheco de Almeida Prado Neto <lucianodeciopacheco@gmail.com>
2024-11-16 15:55:20 -0300

    ajuste para Vercel

--- server-backend/index.js
+++ server-backend/index.js
@@ -3,10 +3,17 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const PORT = 3000
+//Rodar Local:
+// const PORT = 3000
 
 server.use(middlewares)
 server.use(router)
-server.listen(PORT, () => {
-  console.log('Servidor JSON está rodando na Porta: ' + PORT)
+//Rodar Local:
+// server.listen(PORT, () => {
+//   console.log('Servidor JSON está rodando na Porta: ' + PORT)
+// })
+
+//Rodar Servidor Vercel:
+server.listen(() => {
+    console.log('Servidor JSON está rodando')
 })
