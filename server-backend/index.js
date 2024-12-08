36b4e26a700606a94e1019f4e195a5666c7c76ae
Luciano Decio Pacheco de Almeida Prado Neto <lucianodeciopacheco@gmail.com>
2024-12-08 14:32:34 -0300

    Ajustes Projeto

--- server-backend/index.js
+++ server-backend/index.js
@@ -1,22 +1,20 @@
 //Local:
-/*
-const express = require('express');
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-const PORT = 3000
-
-server.use(middlewares)
-server.use(router)
-
-server.listen(PORT, () => {
-  console.log('Servidor JSON está rodando na Porta: ' + PORT)
-})
-
-module.exports = server;
-*/
+// const express = require('express');
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+//
+// const PORT = 3000
+//
+// server.use(middlewares)
+// server.use(router)
+//
+// server.listen(PORT, () => {
+//   console.log('Servidor JSON está rodando na Porta: ' + PORT)
+// })
+//
+// module.exports = server;
 
 //Versel:
 
